086abce4ed95225b1687a9160739ef0d1568cf8f
felipeYule <andrefeceyu@gmail.com>
2016-05-03 11:19:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
 
-session_start();
-// ---
-// la tarea de este archivo es eliminar todo rastro de cookie
 
-// -- eliminamos el usuario
-if(isset($_SESSION['client_id'])){
-	unset($_SESSION['client_id']);
-}
-
-session_destroy();
-
-//estemos donde estemos nos redirije al index
-print "<script>window.location='./';</script>";
 ?>
