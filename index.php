41e8b1452d6841c0608fc7ae98bbbcdb9aaed04a
felipeYule <andrefeceyu@gmail.com>
2016-05-03 11:18:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,18 @@ include "core/autoload.php";
 $lb = new Lb();
 $lb->loadModule("index");
 
+
+session_start();
+// ---
+// la tarea de este archivo es eliminar todo rastro de cookie
+
+// -- eliminamos el usuario
+if(isset($_SESSION['client_id'])){
+	unset($_SESSION['client_id']);
+}
+
+session_destroy();
+
+//estemos donde estemos nos redirije al index
+print "<script>window.location='./';</script>";
 ?>
